fb60a6a2992e8dbf4e0143d3d8eb522913c479f1
NedDinev <ned.dinev@gmail.com>
2023-01-24 16:33:41 +0200

    Fix cubes id

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -4,7 +4,6 @@ const path = require("path");
 
 class Cube {
   constructor(name, description, imgUrl, difficultyLevel) {
-    this.id = db.cubes[db.cubes.length - 1].id + 1;
     this.name = name;
     this.description = description;
     this.imgUrl = imgUrl;
@@ -12,6 +11,8 @@ class Cube {
   }
 
   static save(cube) {
+    const cubeId = db.cubes.length + 1;
+    cube.id = cubeId;
     db.cubes.push(cube);
     const jsonData = JSON.stringify(db, null, 2);
     fs.writeFileSync(path.resolve(__dirname, "../db.json"), jsonData);

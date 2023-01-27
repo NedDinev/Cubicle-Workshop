36bb0c1979a47c66382f8ab025a30663f2745f7e
NedDinev <ned.dinev@gmail.com>
2023-01-27 19:57:09 +0200

    Save cube to DB

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,15 +1,15 @@
-const Cube = require("../models/Cube_old");
+const Cube = require("../models/Cube");
 const db = require("../db.json");
 
 exports.getCreateCube = (req, res) => {
   res.render("create");
 };
 
-exports.postCreateCube = (req, res) => {
+exports.postCreateCube = async (req, res) => {
   //save cube
   const { name, description, imageUrl, difficultyLevel } = req.body;
-  let cube = new Cube(name, description, imageUrl, difficultyLevel);
-  Cube.save(cube);
+  let cube = new Cube({name, description, imageUrl, difficultyLevel});
+  await cube.save();
   //redirect
   res.redirect("/");
 };

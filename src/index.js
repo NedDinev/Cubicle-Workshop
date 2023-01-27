b71cf5a579a61784a623fc7d5ade7ce03fcca0cd
NedDinev <ned.dinev@gmail.com>
2023-01-27 17:44:39 +0200

    Initialize database

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ const express = require("express");
 const routes = require("./routes");
 const config = require("./config");
 const setupViewEngine = require("./config/viewEngine");
+const initDatabase = require("./config/databaseInit");
 
 const app = express();
 setupViewEngine(app);
 
-app.use(express.static("src/public"));
+app.use(express.static("src/public")); // initializes public files
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}...`);
-});
+initDatabase() //if database doesn't exist, don't run server
+  .then(() =>
+    app.listen(config.PORT, () => {
+      console.log(`Server running on port ${config.PORT}...`);
+    })
+  )
+  .catch((err) => console.log(err));

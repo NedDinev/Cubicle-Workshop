5b6527421b0ed4c4c1fac2a0469d13049b1e844e
NedDinev <ned.dinev@gmail.com>
2023-01-30 17:12:38 +0200

    Create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
 
+const Accessory = require("../models/Accessory");
 //URL: /accessory/create
 router.get("/create", (req, res) => {
   res.render("accessory/create");
+});
+
+router.post("/create", async (req, res) => {
+  const { name, description, imageUrl } = req.body; //get form data
+  let accessory = new Accessory({ name, description, imageUrl }); //use form data in accessory schema
+  await accessory.save(); //save new accessory document to database
+
+  //redirect
+  res.redirect("/");
 });
 
 module.exports = router;

808c092cd52fc9f8e9f3dd208d888b84e89417f1
NedDinev <ned.dinev@gmail.com>
2023-01-30 14:30:10 +0200

    delete unneeded code

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,4 @@
 const Cube = require("../models/Cube");
-const db = require("../db.json");
 
 exports.getCreateCube = (req, res) => {
   res.render("create");
@@ -7,15 +6,15 @@ exports.getCreateCube = (req, res) => {
 
 exports.postCreateCube = async (req, res) => {
   //save cube
-  const { name, description, imageUrl, difficultyLevel } = req.body;
   let cube = new Cube({ name, description, imageUrl, difficultyLevel });
   await cube.save();
+
   //redirect
   res.redirect("/");
 };
 
 exports.getDetailsPage = async (req, res) => {
-  let cube = await Cube.findById(req.params.cubeId).lean();
+  let cube = await Cube.findById(req.params.cubeId).lean(); //get cube id from url
   if (!cube) {
     return res.redirect("/404");
   }

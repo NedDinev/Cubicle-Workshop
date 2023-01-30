ba2ac80f7ae88fb7ec84be128e776c9688c36bc1
NedDinev <ned.dinev@gmail.com>
2023-01-30 15:24:34 +0200

    Add accessory controller

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const cubeController = require("./controllers/cubeController");
 const homeController = require("./controllers/homeController");
+const accessoryController = require("./controllers/accessoryController");
 
 router.get("/", homeController.getHomePage);
 router.get("/about", homeController.getAboutPage);
@@ -9,7 +10,8 @@ router.get("/404", homeController.getErrorPage);
 
 router.get("/create", cubeController.getCreateCube);
 router.post("/create", cubeController.postCreateCube);
-
 router.get("/details/:cubeId", cubeController.getDetailsPage);
+
+router.use("/accessory", accessoryController);
 
 module.exports = router;

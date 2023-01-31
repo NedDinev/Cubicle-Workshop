821939bba58cb62b7e4e88931384158a4d342807
NedDinev <ned.dinev@gmail.com>
2023-01-31 17:01:03 +0200

    Add http/https validation with Regex

--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const accessorySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  imageUrl: { type: String, required: true }, //TODO: http/https validation
+  imageUrl: {
+    type: String,
+    required: true,
+    match: [/^https?:\/\//, "Invalid URL"],
+  }, // http/https validation with Regex
   description: { type: String, required: true, maxLength: 50 },
-  //cube
 });
 
 const Accessory = mongoose.model("Accessory", accessorySchema);
--- src/models/Cube.js
+++ src/models/Cube.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cubeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true, maxLength: 50 },
-  imageUrl: { type: String, required: true }, //add http/https validations
+  imageUrl: { type: String, required: true, match: [/^https?:\/\//,"Invalid URL"] }, // http/https validations with RegEx
   difficultyLevel: { type: Number, required: true, $gte: 1, $lte: 6 },
   accessories: [{ type: mongoose.Types.ObjectId, ref: "Accessory" }],
 });

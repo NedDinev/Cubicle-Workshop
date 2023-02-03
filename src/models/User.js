b55d8d3dc3bfdc4a0f78d3493410fe116a01f6c5
NedDinev <ned.dinev@gmail.com>
2023-02-03 15:37:42 +0200

    Register user

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    minLength: [2, "username is too short"],
+    required: true,
+  },
+  password: {
+    type: String,
+    minLength: [6, "password is too short"],
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function () {
+  await bcrypt.hash(this.password, 10).then((hash) => {
+    this.password = hash;
+  });
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
